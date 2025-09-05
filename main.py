d7e8d70b9fe513ca2641cbae41b0a1e24a84ac43
prerana patil <136431517+preranapat@users.noreply.github.com>
2025-09-05 18:54:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ FRONTEND_URL = "https://hosted-app.netlify.app/"
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],  # Allow only your frontend domain for security
+    allow_origins=["*"],  # Allow only your frontend domain for security
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
